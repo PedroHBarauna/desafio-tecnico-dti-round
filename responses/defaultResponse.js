e8cf802c416a658f84b2bfeb874360b0fb2aa604
PedroHBarauna <barauna38@gmail.com>
2022-08-13 12:29:16 -0300

    adicionando a response padrão da api

---
+++ responses/defaultResponse.js
@@ -0,0 +1,7 @@
+let defaultResponse = {
+    'author_link': 'https://github.com/PedroHBarauna',
+    'documentation_link': 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
+    'repository_link'  : 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
+};
+
+module.exports = defaultResponse;
