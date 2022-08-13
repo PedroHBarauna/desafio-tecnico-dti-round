ec1aeb64f65c1841c1b596e64617c4a687e39c4d
PedroHBarauna <barauna38@gmail.com>
2022-08-13 12:28:30 -0300

    adicionando teste para definir uma resposta padrão

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,8 +2,13 @@ import request from 'supertest';
 import app from '../app';
 
 describe("app.js", () =>{
-    test("request a hello world", async ()=>{
-       const res = await request(app).get("/");
-       expect(res.text).toEqual('Hello World'); 
-    })
+    test("request a object with the author's link, doc link and repository link", async ()=>{
+        const res = await request(app).get("/");
+        const defaultResponse = {
+            'author_link': 'https://github.com/PedroHBarauna',
+            'documentation_link': 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
+            'repository_link'  : 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
+        }
+        expect(res.body).toEqual(defaultResponse); 
+     })
 })
