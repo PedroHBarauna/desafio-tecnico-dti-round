4d0184f1a45d1b76d36817c9784f374b5885861c
PedroHBarauna <barauna38@gmail.com>
2022-08-14 02:58:43 -0300

    adicionando testes das classes

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,14 +1,35 @@
-import request from 'supertest';
-import app from '../app';
+import City from '../public/javascripts/City'
+import Status from '../public/javascripts/Status';
 
-describe("app.js", () =>{
-    test("request a object with the author's link, doc link and repository link", async ()=>{
-        const res = await request(app).get("/");
-        const defaultResponse = {
-            'author_link': 'https://github.com/PedroHBarauna',
-            'documentation_link': 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
-            'repository_link'  : 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
-        }
-        expect(res.body).toEqual(defaultResponse); 
-     })
+describe("City.js", () =>{
+    const city = new City(1, 'São Paulo', 32, 10);
+    it("create the class City", () =>{
+        expect(city.id).toEqual(1);
+        expect(city.name).toEqual('São Paulo');
+        expect(city.temp).toEqual(32);
+        expect(city.humidity).toEqual(10);
+    });
+});
+
+describe("Status.js", ()=>{
+    const statusHumidityLow = new Status(30, 9);
+    const statusTempHigh = new Status(31, 31);
+    const statusNoRisk = new Status(29, 45);
+    const statusIntenseCold = new Status(9, 40);
+    const statusUnknow = new Status(30, 30);
+    it("should return 'Umidade baixa'", () =>{
+        expect(statusHumidityLow.build().getStatus()).toEqual("Umidade baixa");
+    })
+    it("should return 'Risco de Ensolação'", () =>{
+        expect(statusTempHigh.build().getStatus()).toEqual("Risco de ensolação");
+    })
+    it("should return 'Nenhum risco iminente'", () =>{
+        expect(statusNoRisk.build().getStatus()).toEqual("Nenhum risco iminente");
+    })
+    it("should return 'Frio intenso'", () =>{
+        expect(statusIntenseCold.build().getStatus()).toEqual("Frio intenso");
+    })
+    it("should return 'Status desconhecido'", () =>{
+        expect(statusUnknow.build().getStatus()).toEqual("Status desconhecido");
+    })
 })
