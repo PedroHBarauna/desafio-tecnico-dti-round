caadf3434d473ae359397e13e32952fd70f11d1a
PedroHBarauna <barauna38@gmail.com>
2022-08-14 18:55:37 -0300

    adicionando o chamado via nome e alterando a criação da classe no chamado via coordenadas

--- routes/callCityFromName.js
+++ routes/callCityFromName.js
@@ -1,6 +1,17 @@
-const apiKey = b5e93b808524f0baa240d0dbc59c991c;
-const unit = metric;
-const https = require('https');
+const request = require('request');
+const City = require('../public/javascripts/City');
+const apiKey = "b5e93b808524f0baa240d0dbc59c991c";
+const unit = "metric";
 
-_EXTERNAL_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const callCityFromName = (name, callback) => {
+    _EXTERNAL_URL_WITH_INFOS = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=${unit}`
+    request(_EXTERNAL_URL_WITH_INFOS, {json:true}, (err, res, body)=> {
+        if(err){
+            return callback(err);
+        }
+        const city = new City(body.id, body.coords.lat, body.coords.lon, body.name, body.main.temp, body.main.humidity);
+        return callback(city);
+    });
+}
 
+module.exports = callCityFromName;
