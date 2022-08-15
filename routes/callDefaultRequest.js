a6cd4af65e44a879d01dc9288cd1923673a8db4a
PedroHBarauna <barauna38@gmail.com>
2022-08-14 22:21:23 -0300

    finalizando as últimas alterações no Docker e renomeando pastas

--- routes/callDefaultRequest.js
+++
@@ -1,7 +0,0 @@
-const callDefaultRequest = {
-    'author_link': 'https://github.com/PedroHBarauna',
-    'documentation_link': 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
-    'repository_link'  : 'https://github.com/PedroHBarauna/desafio-tecnico-dti-round',
-};
-
-module.exports = callDefaultRequest;
