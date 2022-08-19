182665f2a7843238b69c7b54decae004273333cb
PedroHBarauna <barauna38@gmail.com>
2022-08-18 23:44:55 -0300

    adicionando um tratamento de erros trycatch

--- src/endpoints.js
+++ src/endpoints.js
@@ -9,22 +9,31 @@ module.exports = function (app){
     });
 
     app.get("/name/:name", (req, res) => {
-        const name = req.params.name;
-        res.set({ 'content-type': 'application/json; charset=utf-8' });
-        callCityFromName(name, function(response){
-            res.write(JSON.stringify(response));
-            res.end();
-        });
+        try{
+            const name = req.params.name;
+            res.set({ 'content-type': 'application/json; charset=utf-8' });
+            callCityFromName(name, function(response){
+                res.write(JSON.stringify(response));
+                res.end();
+            });
+        }
+        catch(err){
+            res.send(err);
+        }
     });
 
     app.get("/coords/:lat/:lon", (req, res) => {
-        const lat = req.params.lat;
-        const lon = req.params.lon;
-        res.set({ 'content-type': 'application/json; charset=utf-8' });
-        callCityFromCoords(lat, lon, function(response){
-            res.write(JSON.stringify(response));
-            res.end();
-        });
-
+        try{
+            const lat = req.params.lat;
+            const lon = req.params.lon;
+            res.set({ 'content-type': 'application/json; charset=utf-8' });
+            callCityFromCoords(lat, lon, function(response){
+                res.write(JSON.stringify(response));
+                res.end();
+            });
+        }
+        catch(err){
+            res.send(err);
+        }
     })
 }
