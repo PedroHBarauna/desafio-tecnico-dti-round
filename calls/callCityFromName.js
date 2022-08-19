19b1394f8173559127436aa3e9aadfc14eeb09fe
PedroHBarauna <barauna38@gmail.com>
2022-08-18 23:44:17 -0300

    adicionando variáveis de ambiente para deixar mais seguro

--- calls/callCityFromName.js
+++ calls/callCityFromName.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const City = require('../classes/javascripts/City');
 const Status = require('../classes/javascripts/Status')
-const apiKey = "b5e93b808524f0baa240d0dbc59c991c";
+const apiKey = process.env.APIKEY;
 const unit = "metric";
 const lang = "en";
 
@@ -9,7 +9,7 @@ const callCityFromName = (name, callback) => {
     _EXTERNAL_URL_WITH_INFOS = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${apiKey}&units=${unit}&lang=${lang}`
     request(_EXTERNAL_URL_WITH_INFOS, {json:true}, (err, res, body)=> {
         if(body.cod!=200){
-            return callback(body);
+            throw callback(body);
         }
         else{
             const status = new Status(body.main.temp, body.main.humidity).build().getStatus();
